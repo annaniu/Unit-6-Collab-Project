bf00121c5d00391e4df64301c8d8ef985295454b
annaniu <51932257+annaniu@users.noreply.github.com>
2019-06-18 19:56:51 -0700

    Update Unit 6 Collab Project.py

--- Unit 6 Collab Project.py
+++ Unit 6 Collab Project.py
@@ -1,16 +1,16 @@
 # Tic Tac Toe
 
-# To choose the starting player randomly
+# Imported so that later on the starting player can be chosen randomly
 import random
 
-#class Board
+# Makes the class Board
 class Board:
 
-    # to initialize the object
+    # Initializes the object
     def __init__(self):
         self.positions = [' '] * 10
 
-
+    # Creates the visual game board
     def printBoard(self):
         # "board" is a list of 10 strings representing the board (ignore index 0)
         print('   |   |')
@@ -25,18 +25,17 @@ class Board:
         print(' ' + self.positions[1] + ' | ' + self.positions[2] + ' | ' + self.positions[3])
         print('   |   |')
 
-
+     # Checks if there is open spot
     def checkOpenPos(self, i):
-        #If there is open spot
         return self.positions[i] == ' '
 
-    # Check if any 3 in a row have that specific symbol
-    # In order, we check for
+    # Checks if any 3 in a row have the same symbol
+    # In order, we check from
     # Top 3, Middle 3, Bottom 3
     # Left 3, Middle 3, Right 3
     # Left Diagonal, Right Diagonal
     def isWon(self, sym):
-        # check if the given symbol is won
+        # Checks if the given symbol won
         return ((self.positions[7] == sym and self.positions[8] == sym and self.positions[9] == sym) or
         (self.positions[4] == sym and self.positions[5] == sym and self.positions[6] == sym) or
         (self.positions[1] == sym and self.positions[2] == sym and self.positions[3] == sym) or
@@ -53,23 +52,24 @@ class Board:
                 return False
         return True
 
-    # Checks if there is a final tie or if the game is to keep going
+    # Checks if there is a final tie or if the game should keep playing
     def tie(self, sym1, sym2):
         return self.fullBoard() and not self.isWon(sym1) and not self.isWon(sym2)
 
-# class Player
+# Makes the class Player
 class Player:
 
+    # Initializes the object
     def __init__(self, name, symbol):
         self.name = name
         self.symbol = symbol
 
-    # Pass position i and Board object b
+    # Passes position i and Board object b
     def makeMove(self, i, b):
         b.positions[i] = self.symbol
 
-    def getPlayerMove(self, board):
-        # Let the player type in his move.
+    # Lets the player type in their move
+    def getPlayerMove(self, board): 
         move = ' '
         while move not in '1 2 3 4 5 6 7 8 9'.split() or not board.checkOpenPos(int(move)):
             print('What is your next move, ' + self.name + '? (1-9)')
@@ -78,77 +78,78 @@ class Player:
 
 print('Welcome to Tic Tac Toe!')
 
-#create Player 1 object.
+# Creates the object Player 1 and designates them a symbol
 player1 = Player("Player 1", 'X')
-#create Player 2 object.
+# Creates the object Player 2 and designates them a symbol
 player2 = Player("Player 2", 'O')
 
-#create board object.
+# Creates the object board
 board = Board()
 
+# Utilizes the random module to randomly determine which player goes first
 def whoGoesFirst():
-    # randomly choose the player who goes first.
     if random.randint(0, 1) == 0:
         return 'player 2'
     else:
         return 'player 1'
 
+# Allows the player to choose if they would like to play again
 def playAgain():
     # This function returns True if the player wants to play again, otherwise it returns False.
     print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
 
-#game starts
+# The game starts
 while True:
-    #select randomly the player.
+    # Randomly selects the first player
     turn = whoGoesFirst()
     print(turn + ' will go first.')
     gameIsPlaying = True
-    #loop until players are playing the game.
+    # Game loop as the players are playing the game
     while gameIsPlaying:
         if turn == 'player 1':
-            # Player 1's turn.
+            # Player 1's turn
             board.printBoard()
-            #Player inputs his/her move.
+            # Player inputs their move
             move = player1.getPlayerMove(board)
-            #Make Player 1 move.
+            # Executes Player 1's move
             player1.makeMove(move,board)
-            #Check if Player 1 has won.
+            # Checks if Player 1 has won.
             if board.isWon(player1.symbol):
-                #Print board.
+                # Prints board.
                 board.printBoard()
                 print('Hooray! Player 1 has won the game!')
                 gameIsPlaying = False
-            else: #Game is a tie.
+            else: # Game is a tie.
                 if board.fullBoard():
                     board.printBoard()
                     print('The game is a tie!')
                     break
-                else:
+                else: # Game goes to Player 2's turn
                     turn = 'player 2'
 
         else:
             # Player 2's turn.
             board.printBoard()
-            #Player inputs his/her move.
+            # Player inputs their move
             move = player2.getPlayerMove(board)
-            #Make Player 2 move.
+            # Executes Player 2's move
             player2.makeMove(move, board)
-            #Check if Player 2 has won.
+            # Checks if Player 2 has won
             if board.isWon(player2.symbol):
-                #Print board.
+                # Prints board
                 board.printBoard()
                 print('Hooray! Player 2 has won the game!')
                 gameIsPlaying = False
-            else:
+            else: # Game is a tie
                 if board.fullBoard():
                     board.printBoard()
                     print('The game is a tie!')
                     break
-                else:
+                else: # Game goes to Player 1's turn
                     turn = 'player 1'
-     #Clear the board if the game is over.
+     # Clears the board if the game is over
     board = Board()
-    #Check if players want to play again.
+    # Checks if players want to play again
     if not playAgain():
         break
